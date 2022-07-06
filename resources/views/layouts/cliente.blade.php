94c3c36a64532dc75ad41278a70110a07db1fa70
Jesús <jesus777.jsgl@gmail.com>
2022-07-05 20:10:55 -0500

    Layout para usuario de tipo cliente Incluimos un nav preliminar

---
+++ resources/views/layouts/cliente.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+	<meta charset="utf-8">
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1"
+	>
+	<meta
+		name="csrf-token"
+		content="{{ csrf_token() }}"
+	>
+
+	<title>{{ config('app.name', 'Laravel') }}</title>
+
+	<!-- Fonts -->
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"
+	>
+
+	<!-- Styles -->
+	<link
+		rel="stylesheet"
+		href="{{ asset('css/app.css') }}"
+	>
+
+	<!-- Scripts -->
+	<script
+	 src="{{ asset('js/app.js') }}"
+	 defer
+	></script>
+
+	{{-- styles custom --}}
+	{{ $styles ?? '' }}
+</head>
+
+<body class="font-sans antialiased">
+	<div class="min-h-screen bg-gray-100">
+		@include('partials._nav_cliente')
+
+		<!-- Page Heading -->
+		<header class="bg-white shadow">
+			<div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+				{{ $header }}
+			</div>
+		</header>
+
+		<!-- Page Content -->
+		<main>
+			<div class="py-12">
+				<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+					<div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+						{{-- <div class="bg-white shadow-sm sm:rounded-lg"> --}}
+						<div class="app border-b border-gray-200 bg-white p-6">
+							@include('partials._messages')
+							{{ $slot }}
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+	</div>
+	{{-- scripts custom --}}
+	{{ $scripts ?? '' }}
+</body>
+
+</html>
