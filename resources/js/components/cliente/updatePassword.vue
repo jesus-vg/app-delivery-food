5a447e43ee296cc3641b68405b0c3aa9d1b5af23
Jesús <jesus777.jsgl@gmail.com>
2022-07-07 23:17:24 -0500

    Componente para validar cambio de contraseña hay un campo de contraseña y otro para confirmar hay mensaje de validacion

---
+++ resources/js/components/cliente/updatePassword.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="mb-4">
+        <label for="password" class="label">Tu contraseña</label>
+        <input
+            type="password"
+            name="password"
+            id="password"
+            class="input-form"
+            v-model="user.password"
+        />
+    </div>
+    <div class="mb-4">
+        <label for="confirmedPassword" class="label">
+            Confirma tu contraseña
+        </label>
+        <input
+            type="password"
+            name="confirmedPassword"
+            id="confirmedPassword"
+            class="input-form"
+            v-model="user.confirmedPassword"
+        />
+    </div>
+    <p class="text-sm text-red-500" v-if="error !== ''">{{ error }}</p>
+    <p class="text-sm text-red-500" v-if="oldError !== ''">
+        {{ oldError }}
+    </p>
+</template>
+
+<script>
+export default {
+    props: {
+        oldError: {
+            type: String,
+        },
+    },
+    data() {
+        return {
+            user: {
+                password: "",
+                confirmedPassword: "",
+            },
+            error: "",
+        };
+    },
+    methods: {
+        validaPassword() {
+            if (this.user.password !== this.user.confirmedPassword) {
+                this.error =
+                    "Las contraseñas no coniciden, por favor verifique";
+            } else {
+                this.error = "";
+            }
+        },
+    },
+    watch: {
+        "user.password": ["validaPassword"],
+        "user.confirmedPassword": ["validaPassword"],
+    },
+};
+</script>
