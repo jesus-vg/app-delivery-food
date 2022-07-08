4f3496f19d0f569c51aedb0f94cfd960753f326f
Jesús <jesus777.jsgl@gmail.com>
2022-07-07 23:26:00 -0500

    Archivo de rutas con varios cambios. No se registraron los cambios en su momento :v

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Http\Controllers\AlimentoController;
+use App\Http\Controllers\BebidaController;
+use App\Http\Controllers\CategoriaAlimentoController;
+use App\Http\Controllers\CategoriaBebidaController;
+use App\Http\Controllers\ClienteController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmpresaController;
+use App\Http\Controllers\ImagenController;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,14 +20,109 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::middleware( ['auth', 'user_admin'] )
+    ->group( function () {
+        Route::get( '/dashboard', function () {
+            return to_route( 'empresa.edit' );
+        } )->name( 'dashboard' );
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+        Route::controller( EmpresaController::class )
+            ->prefix( 'empresa' )
+            ->group( function () {
+                Route::get( '/edit', 'edit' )->name( 'empresa.edit' );
+                Route::put( '/{empresa}', 'update' )->name( 'empresa.update' );
+            } );
 
-require __DIR__.'/auth.php';
+        Route::controller( CategoriaAlimentoController::class )
+            ->prefix( 'alimentos/categorias' )
+            ->name( 'categoria_alimentos.' )
+            ->group( function () {
+                Route::get( '/', 'index' )->name( 'index' );
+                Route::post( '/', 'store' )->name( 'store' );
+                Route::get( '/{categoria:slug}', 'show' )->name( 'show' );
+                Route::put( '/{categoria}', 'update' )->name( 'update' );
+                Route::delete( '/{categoria}', 'destroy' )->name( 'destroy' );
+            } );
+
+        Route::controller( AlimentoController::class )
+            ->prefix( 'alimentos' )
+            ->name( 'alimentos.' )
+            ->group( function () {
+                Route::get( '/', 'index' )->name( 'index' );
+                Route::get( '/crear', 'create' )->name( 'create' );
+                Route::post( '/', 'store' )->name( 'store' );
+                Route::get( '/{alimento:slug}/editar', 'edit' )->name( 'edit' );
+                Route::put( '/{alimento}', 'update' )->name( 'update' );
+                Route::delete( '/{alimento}', 'destroy' )->name( 'destroy' );
+            } );
+
+        Route::controller( CategoriaBebidaController::class )
+            ->prefix( 'bebidas/categorias' )
+            ->name( 'categoria_bebidas.' )
+            ->group( function () {
+                Route::get( '/', 'index' )->name( 'index' );
+                Route::post( '/', 'store' )->name( 'store' );
+                Route::get( '/{categoria:slug}', 'show' )->name( 'show' );
+                Route::put( '/{categoria}', 'update' )->name( 'update' );
+                Route::delete( '/{categoria}', 'destroy' )->name( 'destroy' );
+            } );
+
+        Route::controller( BebidaController::class )
+            ->prefix( 'bebidas' )
+            ->name( 'bebidas.' )
+            ->group( function () {
+                Route::get( '/', 'index' )->name( 'index' );
+                Route::get( '/crear', 'create' )->name( 'create' );
+                Route::post( '/', 'store' )->name( 'store' );
+                Route::get( '/{bebida:slug}', 'show' )->name( 'show' );
+                Route::get( '/{bebida:slug}/editar', 'edit' )->name( 'edit' );
+                Route::put( '/{bebida}', 'update' )->name( 'update' );
+                Route::delete( '/{bebida}', 'destroy' )->name( 'destroy' );
+            } );
+
+        Route::controller( ClienteController::class )
+            ->prefix( 'clientes' )
+            ->name( 'clientes.' )
+            ->group( function () {
+                Route::get( '/', 'index' )->name( 'index' );
+                Route::get( '/{cliente}', 'show' )->name( 'show' );
+                Route::delete( '/{cliente}', 'destroy' )->name( 'destroy' );
+            } );
+
+    } );
+
+Route::middleware( ['auth', 'user_cliente'] )
+    ->group( function () {
+        Route::controller( ClienteController::class )
+            ->prefix( 'cuenta' )
+            ->name( 'cuenta.' )
+            ->group( function () {
+                Route::get( '/', 'edit' )->name( 'edit' );
+                Route::put( '/{user}', 'update' )->name( 'update' );
+                Route::delete( '/{user}', 'destroy' )->name( 'destroy' );
+            } );
+
+        // Route::get( '/cuenta', [ClienteController::class, 'edit'] )->name( 'cuenta.edit' );
+        // Route::put( '/cuenta/{user}', [ClienteController::class, 'update'] )->name( 'cuenta.update' );
+        // Route::delete( '/cuenta/{user}', [ClienteController::class, 'destroy'] )->name( 'cuenta.destroy' );
+    } );
+
+require __DIR__ . '/auth.php';
+
+// rutas libres
+
+Route::get( '/', function () {
+    return view( 'welcome' );
+} );
+
+Route::controller( ImagenController::class )->prefix( 'imagen' )->group( function () {
+    Route::get( '/{path?}', 'getImagenMini' )->name( 'imagen.getImagenMini' );
+} );
+
+Route::controller( AlimentoController::class )
+    ->prefix( 'alimentos' )
+    ->group( function () {
+        Route::get( '/{alimento:slug}', 'show' )->name( 'alimentos.show' );
+    } );
