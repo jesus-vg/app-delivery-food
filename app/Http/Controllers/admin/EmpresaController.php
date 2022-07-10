d860294c3c19ccc22e098e285833194155dbf144
Jesús <jesus777.jsgl@gmail.com>
2022-07-09 23:24:38 -0500

    Se eliminaron metodos no implementados index created delete

--- app/Http/Controllers/admin/EmpresaController.php
+++ app/Http/Controllers/admin/EmpresaController.php
@@ -5,40 +5,9 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\EmpresaUpdateRequest;
 use App\Models\Empresa;
-use Illuminate\Http\Request;
 
 class EmpresaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request    $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store( Request $request )
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
@@ -72,10 +41,9 @@ class EmpresaController extends Controller
             'hora_apertura',
             'hora_cierre' )
             ->whereId( 1 )
-            ->limit( 1 )
             ->first(); // que nos devuelva solo un elemento y no un array con get()
 
-        return view( 'empresa.edit', [
+        return view( 'admin.empresa.edit', [
             'empresa' => $empresa,
         ] );
     }
@@ -98,16 +66,5 @@ class EmpresaController extends Controller
         return to_route( 'empresa.edit', [
             'empresa' => $empresa,
         ] )->with( 'success', 'Los datos de la empresa han sido actualizados' );
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Empresa         $empresa
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy( Empresa $empresa )
-    {
-        //
     }
 }
