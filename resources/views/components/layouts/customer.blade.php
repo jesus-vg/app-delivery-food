6d60ab9473549c537f5138550c5dfc3795ffe744
Jesús <jesus777.jsgl@gmail.com>
2022-07-11 22:46:17 -0500

    Se quita la clase app que usa vue Para evitar conflictos con alpine js

--- resources/views/components/layouts/customer.blade.php
+++ resources/views/components/layouts/customer.blade.php
@@ -69,7 +69,7 @@
 				<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
 					<div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
 						{{-- <div class="bg-white shadow-sm sm:rounded-lg"> --}}
-						<div class="app border-b border-gray-200 bg-white p-6">
+						<div class="border-b border-gray-200 bg-white p-6">
 							@include('partials._messages')
 							{{ $slot }}
 						</div>
