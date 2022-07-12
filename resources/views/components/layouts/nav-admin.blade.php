a2de9a3b2b0bac9a9cedbef4c1d0487cdf8f244f
Jesús <jesus777.jsgl@gmail.com>
2022-07-11 22:53:11 -0500

    Link pagina inicio redundante

--- resources/views/components/layouts/nav-admin.blade.php
+++ resources/views/components/layouts/nav-admin.blade.php
@@ -16,12 +16,6 @@
 				<!-- Navigation Links -->
 				<div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
 					<x-nav-link
-						:href="route('admin.dashboard')"
-						:active="request()->routeIs('admin.dashboard')"
-					>
-						Inicio
-					</x-nav-link>
-					<x-nav-link
 						:href="route('alimentos.index')"
 						:active="request()->routeIs('alimentos.index')"
 					>
